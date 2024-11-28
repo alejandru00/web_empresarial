1bfc345c6547a21d570e73a5b18ed7b1c8b1f685
alejandru00 <alejandrolliber2004@gmail.com>
2024-11-28 17:35:37 +0100

    commit final

--- webempresa/core/views.py
+++ webempresa/core/views.py
@@ -8,9 +8,3 @@ def about(request):
     return render(request, "core/about.html")
 def store(request):
     return render(request, "core/store.html")
-def contact(request):
-    return render(request, "core/contact.html")
-def blog(request):
-    return render(request, "core/blog.html")
-def sample(request):
-    return render(request, "core/sample.html")
